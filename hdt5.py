a1ff2243687efd50257035b787ed6b69133db5b3
Jose Molina <mol14492@uvg.edu.gt>
2016-08-20 20:14:02 -0600

    Update hdt5.py

--- hdt5.py
+++ hdt5.py
@@ -12,22 +12,25 @@ import simpy
 
 
 SEMILLA = 50
-MEMORIA_RAM = simpy.Container(env, init=0, capacity=100)
-MEMORIA_MIN = 1
-MEMORIA_MAX = 10
+CANT_MEMORIA_RAM=100
+MEMORIA_RAM = simpy.Container(env, init=0, capacity=CANT_MEMORIA_RAM)
+MEM_MIN = 1
+MEM_MAX = 10
+INST_MIN = 1
+INST_MAX = 10
 NUMERO_INSTRUCCIONES = 3
 NUMERO_PROCESOS=25
 INTERVAL_CUSTOMERS=10
 
-def proceso(env,cantidad_memoria,tiempo_proceso,num_inst):
+def proceso(env,cantidad_memoria,tiempo_proceso,num_inst,waiting):
 
     ## ---- new ---- ##
     inicio_proceso = env.now
     yield env.timeout(tiempo_proceso)
     yield MEMORIA_RAM.get(cantidad_memoria)
-    print('Se admitio el proceso')
-    print('con un tiempo de:%f'% env.now)
-    print('con una cantidad de memoria de:%f'% cantidad_memoria)
+    print'Se admitio el proceso'
+    print'con un tiempo de:%f'% env.now
+    print'con una cantidad de memoria de:%f'% cantidad_memoria
 
     ## ---- ready ----##
     for i in range(num_inst):
@@ -39,18 +42,32 @@ def proceso(env,cantidad_memoria,tiempo_proceso,num_inst):
                 realizar=cantidad_instrucciones-instrucciones_completas
             env.timeou(realizar/limite)
             instrucciones_completas=instrucciones_completas+realizar
-            print('Esta listo(ready)')
-            print('Instrucciones completadas:%f'%instrucciones_completas)
-            print('Tiempo:%f'%env.now)
+            print'Esta listo(ready)'
+            print'Instrucciones completadas:%f'%instrucciones_completas
+            print'Tiempo:%f'%env.now
             
+        W=waiting
+        
+        if W==1 and instrucciones_completas<cantidad_instrucciones:
 
+        with cola.request() as reqq:
+            yield reqq
+            yield env.timeout(1)
+            t_I_O=env.now()
+            print'Operaciones i/O'
+            print'tiempo de operaciones:%f'% t_I_O
+
+    ##---- exit ----##
     tiempo_total = env.now - inicio_proceso
+    yield MEMORIA_RAM.put(cantidad_memoria)
     print "Tiempo del proceso:%f"% tiempo_total
+    print 'memoria total:%f'%cantidad_memoria
+    print 'FINAL DEL PROCESO.'
 
 
 
 # Se inicia la simulacion
-
+waiting = random.randint(1,2)
 random.seed(SEMILLA)
 env = simpy.Environment()
 inst=simpy.Resource(env,capacity=3)
@@ -60,4 +77,3 @@ counter = simpy.Resource(env, capacity=1)
 ourGen=generador(NUMERO_PROCESOS)
 #env.process(proceso(env))
 #env.run()
-
